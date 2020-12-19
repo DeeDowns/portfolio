c549f4ed8a495b455a75dee1a9f0883f5b58ffe3
DeeDowns <dee_downs@outlook.com>
2020-12-18 19:33:06 -0500

    modified descriptors

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
                     <img src='https://ca.slack-edge.com/ESZCHB482-W013NQ2U1F1-c896d4be006d-512' alt='black and white avatar'/>
                 </div>
                 <div className='blurb-container'>
-                    <h1>Software Engineer | Life-Long Learner | Not Afraid to Switch it up</h1>
+                    <h1>Software Developer | Life-Long Learner | Not Afraid to Be Seen Trying</h1>
                     <p>
                        Hi, my name is Dewandra Downs (everyone calls me Dee), I have been a gymnastics coach, a flight attendant, and now I am a budding software developer. In everything that I do, my goal is to simply help people. Though the method may vary, the mission is the same. Now as I transition into the tech field, I want to build software that makes a positive and meaningful impact on those who use it. 
                        <br/>
