eebca204447728fab18383f3377a390377290005
Dewandra Downs <47228065+DeeDowns@users.noreply.github.com>
2021-03-30 20:56:39 -0400

    remove logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,9 +12,6 @@ const Navigation = () => {
 
         <Navbar className='nav-container'>
             <Nav>
-                <NavItem className='titleHeader'>
-                    <img src='https://www.freelogodesign.org/file/app/client/thumb/b91b05eb-5382-468e-b210-c28ac2ef2c6b_200x200.png?1602211545069' alt='logo with yellow lettering' />
-                </NavItem>
                 <NavItem className='nav-link-container'>
                     <Link
                         to="home" 
